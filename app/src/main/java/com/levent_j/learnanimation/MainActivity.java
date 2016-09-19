360d42798d4553ace3d9692f4f57a04ef0ffd0f0
Levent_J <liwenjing960110@gmail.com>
2016-09-19 21:22:33 +0800

    通过包装类实现了给原生view增加了属性动画

--- app/src/main/java/com/levent_j/learnanimation/MainActivity.java
+++ app/src/main/java/com/levent_j/learnanimation/MainActivity.java
@@ -1,8 +1,13 @@
 package com.levent_j.learnanimation;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -10,6 +15,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
     ImageView demo1;
     ImageView demo2;
+    ImageView demo3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +30,41 @@ public class MainActivity extends AppCompatActivity {
         demo2.setBackgroundResource(R.drawable.anim2);
         AnimationDrawable drawable = (AnimationDrawable) demo2.getBackground();
         drawable.start();
+
+        demo2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Main2Activity.class));
+            }
+        });
+
+        demo3 = (ImageView) findViewById(R.id.iv_demo3);
+//        AnimatorSet set = (AnimatorSet) AnimatorInflater.loadAnimator(this,R.animator.property_animator);
+//        set.setTarget(demo3);
+//        set.start();
+        demo3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewWrapper wrapper = new ViewWrapper(demo3);
+                ObjectAnimator.ofInt(wrapper,"width",500).setDuration(5000).start();
+            }
+        });
+    }
+
+    static class ViewWrapper{
+        private View mTarget;
+
+        public ViewWrapper(View target){
+            mTarget=target;
+        }
+
+        public int getWidth() {
+            return mTarget.getLayoutParams().width;
+        }
+
+        public void setWidth(int width) {
+            mTarget.getLayoutParams().width=width;
+            mTarget.requestLayout();
+        }
     }
 }
